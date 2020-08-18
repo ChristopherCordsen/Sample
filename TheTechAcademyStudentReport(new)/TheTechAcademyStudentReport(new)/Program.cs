045d5fecb10422121cba8b3513be9dc9f0a6a62f
ChristopherCordsen <c-cordsen@hotmail.com>
2020-08-18 13:34:04 -0700

    Commiting

---
+++ TheTechAcademyStudentReport(new)/TheTechAcademyStudentReport(new)/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace TheTechAcademyStudentReport_new_
+{
+    class Program
+    {
+        private const bool V = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Daily Student Report");
+            Console.ReadLine();
+
+
+            // Questions to ask
+            //What course are you on?
+            //What page number?
+            //Do you need help with anything? Please answer “true” or “false”.
+            //Were there any positive experiences you’d like to share? Please give specifics.
+            //Is there any other feedback you’d like to provide? Please be specific.
+            //How many hours did you study today?
+            //After print “Thank you for your answers. An Instructor will respond to this shortly. Have a great day!” 
+
+            Console.WriteLine("What course are on?");
+            string course = Console.ReadLine();
+            
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+
+            Console.WriteLine("Do you need help with anything? Please answer True or False!");
+            string helpStatus = Console.ReadLine();
+            //bool needHelp = V;
+            //var helpStatus = Convert.ToString(value: V);
+            //Console.WriteLine(helpStatus);
+            //Console.ReadLine();
+
+            Console.WriteLine("Where there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudy = Console.ReadLine();
+
+
+
+            //Answers to Questions 
+            Console.WriteLine("Your Answers!");
+            Console.WriteLine("Your course is: " + course); 
+            Console.WriteLine("Your page number is: " + pageNumber);
+            Console.WriteLine("Help true or false: " + helpStatus);
+            Console.WriteLine("Your positive experiences are: " + positiveExperiences);
+            Console.WriteLine("Your feedback is: " + feedback);
+            Console.WriteLine("Your hours worked are: " + hoursStudy);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly.Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VariablesAndDataTypes(new)/VariablesAndDataTypes(new)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace VariablesAndDataTypes_new_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////This is a simple program     (ctrl KC is comments it and to uncomment is ctrl KU)
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine(); //Casting
+            //Console.WriteLine("Your favorite number is: " + favoriteNumber);
+            //Console.Read();
+
+            //Console.WriteLine("What is your favorite number?"); //Math 
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            //char randomLetter = "X"; 
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.234567;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Christopher";
+
+            int currentAge = 37;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+        }
+    }
+}
