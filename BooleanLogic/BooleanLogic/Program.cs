0f89e3a3a905e4237cbdae3f7f2e6f587af3de82
ChristopherCordsen <c-cordsen@hotmail.com>
2020-08-20 02:43:07 -0700

    Commit 54

--- BooleanLogic/BooleanLogic/Program.cs
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -30,13 +30,13 @@ namespace BooleanLogic
             //bool result = (num1 > num2 && (num3 == num4 || num5 > num6));
             //Console.WriteLine(result);
 
-            int age = 31;
-            bool hasticket = true;
-            bool haspassport = true;
+            //int age = 31;
+            //bool hasticket = true;
+            //bool haspassport = true;
 
-            bool canfly = (age >= 18 && hasticket && haspassport);
-            Console.WriteLine(canfly);
-            Console.ReadLine();
+            //bool canfly = (age >= 18 && hasticket && haspassport);
+            //Console.WriteLine(canfly);
+            //Console.ReadLine();
 
             //Console.WriteLine(true && true && true || false);
             //Console.WriteLine(true && false);
@@ -59,8 +59,48 @@ namespace BooleanLogic
             //Console.WriteLine(true ^ false);
             //Console.WriteLine(false ^ false);
 
+            //if (true) ;
+            //{
+            //    Console.WriteLine("Hello!");
+            //}
 
-            Console.WriteLine();
+
+            //if (1 == 1);
+            //{
+            //    Console.WriteLine("1 equals 1!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("1 does not equal 1");
+            //}
+
+            //string name = "Brett";
+
+            //if (name == "Jesse") 
+            //{
+            //    Console.WriteLine("Your name is Jesse");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //}
+
+            string name = "Adam";
+
+            if (name == "Jesse")
+            {
+                Console.WriteLine("Your name is Jesse");
+
+            }
+            else if (name == "Brett")
+            {
+                Console.WriteLine("Your name is not Jessee");
+            }
+            else
+            {
+                Console.WriteLine("Your name is not Jesse. your name is not Brett.");
+            }
+
         }
     }
 }
