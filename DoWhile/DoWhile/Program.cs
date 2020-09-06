c508b913bac37bf8c1d8f1e7251363c55c895ff9
ChristopherCordsen <c-cordsen@hotmail.com>
2020-09-06 10:43:39 -0700

    Commiting DoWhile

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How tall are you in inches? ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            string result = height >= 67 ? "You're tall! " : "sup shorty!";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Guess my age?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                bool isGuessed = number == 37;
+
+                do
+                {
+                    switch (number)
+                    {
+                        case 45:
+                            Console.WriteLine("You guessed 45. Try again.");
+                            Console.WriteLine("Guess my age?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 49:
+                            Console.WriteLine("You guessed 49. Try again.");
+                            Console.WriteLine("Guess my age?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 32:
+                            Console.WriteLine("You guessed 32. Try again.");
+                            Console.WriteLine("Guess my age?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 37:
+                            Console.WriteLine("You are correct, I'm 37!");
+                            isGuessed = true;
+                            break;
+                        default:
+                            Console.WriteLine("Nope, wrong.");
+                            Console.WriteLine("Guess my age?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+                }
+                while (!isGuessed);
+
+                Console.Read();
+            }
+        
+    }
+}
